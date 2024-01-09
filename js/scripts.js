070bc1ab7c916dbd0a97fac26a94034f673ddba0
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2024-01-09 15:06:33 +0100

    - ADD  Vue Instance with Array of object; - ADD function for add new object with ' text '  key

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,55 @@
-// Test funzionamento
-console.log('Template');
+const { createApp } = Vue;
+
+createApp ({
+    data() {
+        return {
+
+            todos : [
+                {
+                    text: 'fare i compiti',
+                    done: false,
+                },
+                {
+                    text: 'andare in palestra',
+                    done: false,
+                },
+                {
+                    text: 'chiamare ufficio',
+                    done: false,
+                },
+            ],
+
+            newTaskInput : '',
+
+        };
+    },
+    methods : {
+
+        addTask(){
+
+            if(this.newTaskInput.length > 2){
+                
+                let newTask = {};
+
+                newTask.text = this.newTaskInput;
+                newTask.done = false;
+                
+                console.log(newTask);
+
+                this.todos.push(newTask);
+
+                this.newTaskInput = '';
+
+
+                console.log(this.todos);
+
+            };
+
+        },
+
+        removeTask(i) {
+            console.log(i, this.todos[i]);
+            this.todos.splice(i, 1);
+        }
+    },
+}).mount('#app');
